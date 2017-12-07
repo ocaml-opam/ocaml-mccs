b429dfb4496ed0976c3662f16694625453540a18
David Allsopp <david.allsopp@metastack.com>
2017-12-07 15:21:32 +0000

    Windows implementation of CTRL+C handling
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

--- src/context_flags.ml
+++ src/context_flags.ml
@@ -12,6 +12,11 @@ match Sys.argv.(1) with
       print_string "(-lstdc++)"
     else
       print_string "()"
+| "flags" ->
+    if Config.system = "mingw" then
+      print_string "(-ccopt \"-link -shared-libgcc\")"
+    else
+      print_string "()"
 | _ ->
     Printf.eprintf "Unrecognised context instruction: %s\n" Sys.argv.(1);
     exit 1
