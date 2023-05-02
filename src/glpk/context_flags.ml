bd11826656dd8e6d9e778510e3318eb142b7893b
Antonin Décimo <antonin@tarides.com>
2023-05-02 21:27:57 +0100

    Try to detect GCC vs Clang for compiler flags

--- src/glpk/context_flags.ml
+++ src/glpk/context_flags.ml
@@ -1,25 +1,44 @@
+let detect_cc =
+  let clang = Str.regexp "clang"
+  and gcc = Str.regexp "Free Software Foundation" in
+  fun () ->
+  let ch = Unix.open_process_in (Config.c_compiler ^ " --version") in
+  let rec aux () =
+    let line = input_line ch in
+    try Str.search_forward clang line 0 |> ignore; `Clang
+    with Not_found ->
+    try Str.search_forward gcc line 0 |> ignore; `Gcc
+    with Not_found ->
+    aux ()
+  in
+  let cc = try aux () with End_of_file -> `Unknown in
+  Unix.close_process_in ch |> ignore;
+  cc
+
 let () =
   match Sys.argv.(1) with
   | "clibs" ->
-      if Sys.win32 && Config.ccomp_type = "msvc" then
-        print_string "(glpk_4_65.lib)"
-      else
-        let glpk =
-          if Array.length Sys.argv > 2 && Sys.argv.(2) = "static" then
-            (* flexlink doesn't (yet) support -l: *)
-            if Sys.win32 then
-              "libglpk.a"
-            else
-              "-l:libglpk.a"
-          else
-            "-lglpk"
-        in
-        Printf.printf "(-lstdc++ %s)" glpk
+     if Sys.win32 && Config.ccomp_type = "msvc" then
+       print_string "(glpk_4_65.lib)"
+     else
+       let glpk =
+         if Array.length Sys.argv > 2 && Sys.argv.(2) = "static" then
+           (* flexlink doesn't (yet) support -l: *)
+           if Sys.win32 then
+             "libglpk.a"
+           else
+             "-l:libglpk.a"
+         else
+           "-lglpk"
+       in
+       Printf.printf "(-lstdc++ %s)" glpk
   | "cflags" ->
-      if Config.ccomp_type = "cc" then
-        print_string "(-Wno-parentheses -Wno-stringop-overflow)"
-      else
-        print_string "()"
+     if Config.ccomp_type = "cc" then
+       match detect_cc () with
+       | `Gcc -> print_string "(-Wno-parentheses -Wno-stringop-overflow)"
+       | `Clang | `Unknown -> print_string "(-Wno-parentheses)"
+     else
+       print_string "()"
   | _ ->
-      Printf.eprintf "Unrecognised context instruction: %s\n" Sys.argv.(1);
-      exit 1
+     Printf.eprintf "Unrecognised context instruction: %s\n" Sys.argv.(1);
+     exit 1
